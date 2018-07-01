42e79de2f5bcebc108674beaba0e2273bd91ddcd
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-01 17:00:57 -0600

    writes & improves guess tests; renamed attributes in guess.rb

--- lib/guess.rb
+++ lib/guess.rb
@@ -3,12 +3,11 @@
 class Guess
   # attr_reader turns instance variables into methods that you can access outside of the class
   # outside of the class, you can read but not write (whereas with attr_writer, someone would be able to access your code)
-  attr_reader :value,# be sure to remember a comma here (but don't comma after the last one)
-              :suit
+  attr_reader :response, :card # be sure to remember a comma here (but don't comma after the last one)
 
-  def initialize(value, suit)
-    @value = value # instance variable
-    @suit = suit # instance variable
+  def initialize(response, card)
+    @response = response # instance variable
+    @card = card
   end
-  
+
 end
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,23 +1,39 @@
 require "./lib/guess"
+require "./lib/card"
 require "minitest/autorun"
 require "minitest/pride"
 require 'pry'
 class GuessTest < Minitest::Test
 
   # test for instance of guess in class Guess
-  def test_for_instance_of_guess1_in_class_Guess
-    guess1 = Guess.new("Ace","Spades")
-    assert_instance_of Guess, guess1
+  # can create a guess object
+  def test_can_create_an_instance_of_guess
+    card = Card.new("10","Hearts")
+    guess = Guess.new("10 of Hearts",card)
+    assert_instance_of Guess, guess
   end
 
-  # test if value and suit methods assigns appropriately
-  def test_if_value_method_assigns_the_variable_guess_an_instance_of_the_class_guess
-    guess1 = Guess.new("Ace","Spades")
-    assert_equal "Ace", guess1.value
+  # test if response and card methods assigns appropriately
+  def test_if_guess_has_a_response # response = attribute
+    card = Card.new("10","Hearts")
+    guess = Guess.new("10 of Hearts",card)
+    assert_equal "10 of Hearts", guess.response
   end
 
-  def test_if_suit_method_assigns_the_variable_guess_an_instance_of_the_class_guess
-    guess1 = Guess.new("Ace","Spades")
-    assert_equal "Spades", guess1.suit
+  def test_if_guess_has_a_card_method
+    card = Card.new("10","Hearts")
+    guess = Guess.new("10 of Hearts",card)
+    # p card
+    # p guess
+    assert_equal card, guess.card
   end
+
+  # testing method correct
+  def test_if_guess_is_correct
+    card = Card.new("10","Hearts")
+    guess = Guess.new("10 of Hearts",card)
+    assert guess.correct? # fail unless guess.correct returns true
+  end
+
+
 end
