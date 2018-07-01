2766175cdd257f4c0f032ddc8ad2ac2d5e12b13f
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-01 13:49:00 -0600

    added an attr reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,14 @@
 # GOAL 1.1: make a card Object
 # GOAL 1.2: initialize it with a value and a suit
 class Card
-
+  # attr_reader turns instance variables into methods that you can access outside of the class
+  # outside of the class, you can read but not write (whereas with attr_writer, someone would be able to access your code)
+  attr_reader :value, # be sure to remember a comma here (but don't comma after the last one)
+              :suit
   def initialize(value, suit)
-    @value = value
-    @suit = suit
+    @value = value # instance variable
+    @suit = suit # instance variable
   end
+
+
 end
