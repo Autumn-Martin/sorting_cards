4f212604dceaa25b039c05f47736682e594ac6eb
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-01 14:32:45 -0600

    added tests verify if suit and value assign appropriately

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,4 +1,4 @@
-require "./lib/card"
+require "./lib/guess"
 require "minitest/autorun"
 require "minitest/pride"
 require 'pry'
@@ -8,5 +8,16 @@ class GuessTest < Minitest::Test
   def test_for_instance_of_guess1_in_class_Guess
     guess1 = Guess.new("Ace","Spades")
     assert_instance_of Guess, guess1
+  end
+
+  # test if value and suit methods assigns appropriately
+  def test_if_value_method_assigns_the_variable_guess_an_instance_of_the_class_guess
+    guess1 = Guess.new("Ace","Spades")
+    assert_equal "Ace", guess1.value
+  end
+
+  def test_if_suit_method_assigns_the_variable_guess_an_instance_of_the_class_guess
+    guess1 = Guess.new("Ace","Spades")
+    assert_equal "Spades", guess1.suit
   end
 end
