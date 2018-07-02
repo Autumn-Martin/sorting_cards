d2e6b2c836111f4bf2ee356cba543c5ded3a3fee
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-01 18:36:23 -0600

    fixed an error

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -41,10 +41,10 @@ class GuessTest < Minitest::Test
     assert_equal ["10", "of", "Hearts"], guess.response_to_array
   end
 
-  def test_about_feedback
+  def test_for_feedback
     card = Card.new("10","Hearts")
     guess = Guess.new("10 of Hearts",card)
-    assert_equal "Correct!", guess.response 
+    assert_equal "Correct!", guess.feedback
   end
 
 end
