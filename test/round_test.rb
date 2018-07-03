84bedfeac82921a84fb5087a5103091865a0169d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 17:22:33 -0600

    made a test for the existance of round

---
+++ test/round_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require 'pry'
+
+class Round_Test < Minitest::Test
+  # create a test for an instance of round
+  # this errors without a round class (uninitialized constant)
+  card_1 = Card.new("3","Hearts")
+  card_2 = Card.new("4", "Clubs")
+  deck = Deck.new([card_1, card_2])
+  round = Round.new(deck)
+  assert_instance_of Round, round
+end
