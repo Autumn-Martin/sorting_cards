367c93b25c7837c6fa81ac5523258a52c4d69185
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-02 21:42:08 -0600

    adds a test for #cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -15,4 +15,14 @@ class Deck_Test < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  # test #cards
+  # this should error without the method #cards
+  def test_can_create_deck_as_an_array_of_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal [card_1, card_2, card_3], deck.cards
+  end
+
 end
