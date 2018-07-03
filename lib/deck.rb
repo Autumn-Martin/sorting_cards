94d7e0799a5327fcbffaf6109e24250f993f2fb2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 09:05:57 -0600

    adds a count method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,16 @@
 # GOAL 2.2: make a deck class
 class Deck
 # GOAL: initialize to set up attributes at the beginning of the objects lifetime
-  def initialize(deck)
-    @deck = [] # instance variable
+  attr_reader :cards
+  
+  def initialize(cards)
+    @cards = cards # instance variable
   end
+
+  # GOAL: add method that returns the cards in the deck (#cards)
+  def count
+    deck.count
+  end
+
+
 end
