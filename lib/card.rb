4c7b53b3add49ae6bd2366e1b315ad2f98b1e307
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 16:49:22 -0600

    adds a space

--- lib/card.rb
+++ lib/card.rb
@@ -5,9 +5,10 @@ class Card
   # outside of the class, you can read but not write (whereas with attr_writer, someone would be able to access your code)
   attr_reader :value, # be sure to remember a comma here (but don't comma after the last one)
               :suit
+
   def initialize(value, suit)
-    @value = value # instance variable
-    @suit = suit # instance variable
+    @value = value # instance variables
+    @suit = suit
   end
 
 
