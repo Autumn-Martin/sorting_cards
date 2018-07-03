57f141c9996be38b4ff49b525a6655cee90b181f
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-02 20:20:17 -0600

    adds cards to deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,7 +7,10 @@ class Deck_Test < Minitest::Test
   # test if you can create an instance of deck
   # this should error without a deck class (uninitialized constant error)
   def test_can_create_an_object_deck
-    deck = Deck.new([card_1, card_2])
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
     assert_instance_of Deck, deck
   end
 
