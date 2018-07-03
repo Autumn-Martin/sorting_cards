045767ec858f99152fe349e48215f17b64714ccc
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 12:42:39 -0600

    comments out #count since I need to write a test first

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,15 +2,15 @@
 class Deck
 # GOAL: initialize to set up attributes at the beginning of the objects lifetime
   attr_reader :cards
-  
+
   def initialize(cards)
     @cards = cards # instance variable
   end
 
-  # GOAL: add method that returns the cards in the deck (#cards)
-  def count
-    deck.count
-  end
+  # # GOAL: add method that returns the cards in the deck (#cards)
+  # def count
+  #   deck.count
+  # end
 
 
 end
