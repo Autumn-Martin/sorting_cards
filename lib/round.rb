aa9f2b603d9bdf442097e6f94febb8edcafe6e34
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 19:32:18 -0600

    adds a current card method

--- lib/round.rb
+++ lib/round.rb
@@ -2,9 +2,18 @@ require "./lib/deck"
 # make a class called Round
 class Round
   # will need an attr_reader
-  attr_reader :deck
+  attr_reader :deck, :guesses
   # initialize
   def initialize(deck)
     @deck = deck
+    @guesses = []
   end
+
+  def current_card
+    @deck.cards.first  
+  end
+  # add method for guesses
+  # def guesses # another way to add a method for guesses
+  #   guesses = []
+  # end
 end
