2cdcd7848f56477c0e5bc1f3e94ea25aed8d6dc6
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 18:21:59 -0600

    amends test for if round exists

--- test/round_test.rb
+++ test/round_test.rb
@@ -3,12 +3,14 @@ require "minitest/autorun"
 require "minitest/pride"
 
 
-class Round_Test < Minitest::Test
+class RoundTest < Minitest::Test
   # create a test for an instance of round
   # this errors without a round class (uninitialized constant)
-  card_1 = Card.new("3","Hearts")
-  card_2 = Card.new("4", "Clubs")
-  deck = Deck.new([card_1, card_2])
-  round = Round.new(deck)
-  assert_instance_of Round, round
+  def test_if_round_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_instance_of Round, round
+  end
 end
