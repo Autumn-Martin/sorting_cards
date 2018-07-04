3e4e3aa3383d7d002f485ccd93a9052d6e9ed34f
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 18:52:52 -0600

    adds a test for guesses method

--- test/round_test.rb
+++ test/round_test.rb
@@ -15,11 +15,20 @@ class RoundTest < Minitest::Test
   end
 
   # test for #deck method
-  def test_deck_method 
+  def test_deck_method_on_round
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
     assert_equal deck, round.deck
+  end
+
+  # test for #guesses method
+  def test_guesses_method_on_round
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
   end
 end
