7be62dd790e396aef174c539905f8a4a264a393a
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-04 12:46:37 -0600

    adds test for getting an incorrect second guess

--- test/round_test.rb
+++ test/round_test.rb
@@ -91,6 +91,16 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     round.record_guess({value: "3", suit: "Hearts"})
     round.record_guess({value: "Jack", suit: "Diamonds"})
-    assert_equal 2, round.guesses.count 
+    assert_equal 2, round.guesses.count
+  end
+
+  def test_for_feedback_on_incorrect_second_guess
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess({value: "3", suit: "Hearts"})
+    round.record_guess({value: "Jack", suit: "Diamonds"})
+    assert_equal "Incorrect.", round.guesses.last.feedback
   end
 end
