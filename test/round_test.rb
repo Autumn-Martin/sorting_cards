357091d9c16f3d061be12061eddc5de645233176
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 19:28:50 -0600

    added a test for the current card method

--- test/round_test.rb
+++ test/round_test.rb
@@ -31,4 +31,12 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     assert_equal [], round.guesses
   end
+
+  def test_for_current_card
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck.cards.first, round.current_card
+  end
 end
