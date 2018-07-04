bd98bc6cb54cc0984383fddb24454f828bab8a78
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-03 18:49:02 -0600

    adds test for deck method

--- test/round_test.rb
+++ test/round_test.rb
@@ -13,4 +13,13 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     assert_instance_of Round, round
   end
+
+  # test for #deck method
+  def test_deck_method 
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+  end
 end
