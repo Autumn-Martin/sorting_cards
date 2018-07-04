f616fe0ff4f86e5b10ab3973d502229bcf980959
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-04 17:49:54 -0600

    switches colons to hashrockets

--- lib/card.rb
+++ lib/card.rb
@@ -16,23 +16,23 @@ class Card
 
   def total_value
     card_values = {
-                "2": 2.0,
-                "3": 3.0,
-                "4": 4.0,
-                "5": 5.0,
-                "6": 6.0,
-                "7": 7.0,
-                "8": 8.0,
-                "9": 9.0,
-                "10": 10.0,
-                "Jack": 11,
-                "King": 12,
-                "Queen": 13,
-                "Ace": 14,
-                "Clubs": 0.1,
-                "Diamonds": 0.2,
-                "Hearts": 0.3,
-                "Spades": 0.4
+                "2" => 2.0,
+                "3"=> 3.0,
+                "4"=> 4.0,
+                "5"=> 5.0,
+                "6"=> 6.0,
+                "7"=> 7.0,
+                "8"=> 8.0,
+                "9"=> 9.0,
+                "10"=> 10.0,
+                "Jack"=> 11,
+                "King"=> 12,
+                "Queen"=> 13,
+                "Ace"=> 14,
+                "Clubs"=> 0.1,
+                "Diamonds"=> 0.2,
+                "Hearts"=> 0.3,
+                "Spades"=> 0.4
               }
     total_value = card_values[value] + card_values[suit]
     return total_value
